d81bfa259d2f3b71cd130ca1c6b75e9e85bb4971
jackie247 <jackieyu247@gmail.com>
2024-09-23 21:57:09 +0100

    Update usersController.js
    add password hashing

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,1 +1,45 @@
-const db = require("../");
+const db = require("../db/queries");
+const bcrypt = require("bcryptjs");
+
+exports.getLogin = async (req, res, next) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    next(err);
+  }
+};
+exports.postLogin = async (req, res, next) => {
+  // TO DO
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getSignup = async (req, res, next) => {
+  try {
+    res.render("signup", { errors: {} });
+  } catch (err) {
+    next(err);
+  }
+};
+exports.postSignup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log("error hashing password");
+      }
+      try {
+        await db.addUser(username, email, hashedPassword);
+      } catch (err) {
+        console.log(err);
+        next(err);
+      }
+    });
+
+    res.redirect("/users/login");
+  } catch (err) {
+    next(err);
+  }
+};
